dcd637470fba2f20c58c4f47101367e887ff64d2
Everardo Guerrero <Everardo.Guerrero@flex.com>
2020-12-03 14:54:56 -0600

    ola

---
+++ sendmail.php
@@ -0,0 +1,32 @@
+<?php
+
+// Please specify your Mail Server - Example: mail.yourdomain.com.
+ini_set("SMTP","gmail.com");
+
+// Please specify an SMTP Number 25 and 8889 are valid SMTP Ports.
+ini_set("smtp_port","8889");
+
+// Please specify the return address to use
+ini_set('sendmail_from', 'evegand.97@gmail.com');
+
+//echo phpinfo();
+
+$to_email = "evegand.97@gmail.com";
+$subject = "Simple Email Test via PHP";
+$body = "Hi,nn This is test email send by PHP Script";
+$headers = "From: sender\'s email";
+
+if (mail($to_email, $subject, $body)) {
+    echo "Email successfully sent to $to_email...";
+} else {
+    echo "Email sending failed...";
+}
+
+/*
+$to = "destinatario@email.com";
+$subject = "Asunto del email";
+$message = "Este es mi primer envío de email con PHP";
+ 
+mail($to, $subject, $message);
+*/
+?>
